4cbb7665c6ce623053a38a95dc36739dafa18114
Codedlio <alexis.leonel_@hotmail.com>
2023-08-08 01:58:37 -0600

    geodata

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MexicoMap from "./components/MexicoMap";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mapa de Geodatos</h1>
+      <MexicoMap />
     </div>
   );
-}
+};
 
 export default App;
